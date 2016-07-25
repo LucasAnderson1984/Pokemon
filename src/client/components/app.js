bf9bf86ab1f8d4873da70a50daf09928252f5dae
Lucas Anderson <landerson_khs@hotmaill.com>
2016-07-24 19:15:26 -0700

    Updated the css for the footer   Rearranged app.js to match the changes for the footer to work correctly

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -5,10 +5,12 @@ import Footer from '../containers/footer';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navigation />
-        <div className='container'>
-          { this.props.children }
+      <div className='main'>
+        <div className='page-wrap'>
+          <Navigation />
+          <div className='container'>
+            { this.props.children }
+          </div>
         </div>
         <Footer />
       </div>
--- src/client/containers/footer.js
+++ src/client/containers/footer.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
 
-export default class Footer extends Component {  
+export default class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="container">
+        <div className="container-fluid">
             <p className="pull-left">Pokedex is &copy; 2016 Lucas Anderson</p>
         </div>
       </footer>
